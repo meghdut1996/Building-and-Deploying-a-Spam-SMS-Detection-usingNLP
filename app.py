22ed1e8d8476102f3bdfa8ab6f7b2744879c37c5
Meghdut Nandy <66274661+meghdut1996@users.noreply.github.com>
2020-09-09 11:17:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,url_for,request
+import pickle
+import joblib
+app=Flask(__name__,template_folder='templates')
+app.config["DEBUG"]=True
+
+filename="model.pkl"
+model=joblib.load(open(filename,'rb'))
+cv=joblib.load(open('transform.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET'])
+def predict():
+    message=request.args['message']
+    data=[message]
+    vect=cv.transform(data).toarray()
+    my_prediction=model.predict(vect)
+    if my_prediction==1:
+        result="It's a Spam message"
+    elif my_prediction==0:
+        result="It's Not a Spam message"
+    return result
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
